127f3cf648c2777999975eafcf9d9e12f13e72a9
rafinurilakbarf <203040135@mail.unpas.ac.id>
2022-12-12 10:03:40 +0700

    video laravel

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+            // User::factory()->create([
+            //     'name' => 'Test User',
+            //     'email' => 'test@example.com',
+            // ]);
+
+
+            User::create([
+                'name' => 'Fresabayu A',
+                'username' => 'fresaba',
+                'email' => 'fresabayu0601@gmail.com',
+                'password' => bcrypt('12345')
+            ]);
+
+            // User::create([
+            //     'name' => 'ST',
+            //     'email' => 'esteee@gmail.com',
+            //     'password' => bcrypt('12345')
+            // ]);
+
+            User::factory(3)->create();
+
+            Category::create([
+                'name' => 'Web Programming',
+                'slug' => 'web-programming'
+
+            ]);
+
+            Category::create([
+                'name' => 'Web Design',
+                'slug' => 'web-design'
+
+            ]);
+
+            Category::create([
+                'name' => 'Personal',
+                'slug' => 'personal'
+
+            ]);
+
+
+            Post::factory(20)->create();
+
+            // Post::create([
+            //     'category_id' => 1,
+            //     'user_id' => 1,
+            //     'title' => 'Judul Pertama',
+            //     'slug' => 'judul-pertama',
+            //     'excerpt' => ' Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate, ratione laudantium fugit tempore ipsa vitae necessitatibus? Vero consectetur reprehenderit cumque.',
+            //     'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui dolorem debitis ab illum atque vero optio voluptas nulla reprehenderit. Molestias provident doloribus eius, fugiat aliquam dolore praesentium magni? Esse temporibus aliquid rem ab veritatis incidunt quas deleniti consequatur architecto beatae, ullam repudiandae necessitatibus pariatur quisquam, iure in dolorum consequuntur laboriosam quidem voluptates ea deserunt vel. Ducimus esse repellendus numquam eligendi! Voluptates voluptas ipsum repellat earum sapiente? Reprehenderit, omnis debitis officia laudantium, perspiciatis accusamus illum soluta fugit impedit, aliquam aperiam eos corrupti architecto ratione quis deserunt dolor quam culpa atque sequi animi iusto in fugiat! Optio, iste. Nihil totam minus deleniti.'
+            // ]);
+
+            // Post::create([
+            //     'category_id' => 1,
+            //     'user_id' => 1,
+            //     'title' => 'Judul Kedua',
+            //     'slug' => 'judul-ke-dua',
+            //     'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate, ratione laudantium fugit tempore ipsa vitae necessitatibus? Vero consectetur reprehenderit cumque.',
+            //    'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui dolorem debitis ab illum atque vero optio voluptas nulla reprehenderit. Molestias provident doloribus eius, fugiat aliquam dolore praesentium magni? Esse temporibus aliquid rem ab veritatis incidunt quas deleniti consequatur architecto beatae, ullam repudiandae necessitatibus pariatur quisquam, iure in dolorum consequuntur laboriosam quidem voluptates ea deserunt vel. Ducimus esse repellendus numquam eligendi! Voluptates voluptas ipsum repellat earum sapiente? Reprehenderit, omnis debitis officia laudantium, perspiciatis accusamus illum soluta fugit impedit, aliquam aperiam eos corrupti architecto ratione quis deserunt dolor quam culpa atque sequi animi iusto in fugiat! Optio, iste. Nihil totam minus deleniti.'
+            // ]);
+
+            // Post::create([
+            //     'category_id' => 2,
+            //     'user_id' => 1,
+            //     'title' => 'Judul Ketiga',
+            //     'slug' => 'judul-ke-tiga',
+            //     'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate, ratione laudantium fugit tempore ipsa vitae necessitatibus? Vero consectetur reprehenderit cumque.',
+            //     'body' => ' Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui dolorem debitis ab illum atque vero optio voluptas nulla reprehenderit. Molestias provident doloribus eius, fugiat aliquam dolore praesentium magni? Esse temporibus aliquid rem ab veritatis incidunt quas deleniti consequatur architecto beatae, ullam repudiandae necessitatibus pariatur quisquam, iure in dolorum consequuntur laboriosam quidem voluptates ea deserunt vel. Ducimus esse repellendus numquam eligendi! Voluptates voluptas ipsum repellat earum sapiente? Reprehenderit, omnis debitis officia laudantium, perspiciatis accusamus illum soluta fugit impedit, aliquam aperiam eos corrupti architecto ratione quis deserunt dolor quam culpa atque sequi animi iusto in fugiat! Optio, iste. Nihil totam minus deleniti.'
+            // ]);
+
+            // Post::create([
+            //     'category_id' => 2,
+            //     'user_id' => 2,
+            //     'title' => 'Judul Keempat',
+            //     'slug' => 'judul-ke-empat',
+            //     'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate, ratione laudantium fugit tempore ipsa vitae necessitatibus? Vero consectetur reprehenderit cumque.',
+            //     'body' => ' Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui dolorem debitis ab illum atque vero optio voluptas nulla reprehenderit. Molestias provident doloribus eius, fugiat aliquam dolore praesentium magni? Esse temporibus aliquid rem ab veritatis incidunt quas deleniti consequatur architecto beatae, ullam repudiandae necessitatibus pariatur quisquam, iure in dolorum consequuntur laboriosam quidem voluptates ea deserunt vel. Ducimus esse repellendus numquam eligendi! Voluptates voluptas ipsum repellat earum sapiente? Reprehenderit, omnis debitis officia laudantium, perspiciatis accusamus illum soluta fugit impedit, aliquam aperiam eos corrupti architecto ratione quis deserunt dolor quam culpa atque sequi animi iusto in fugiat! Optio, iste. Nihil totam minus deleniti.'
+            // ]);
+    }
+
+
+}
+
---
+++ routes/web.php
@@ -0,0 +1,85 @@
+<?php
+
+use App\Http\Controllers\PostController;
+use App\Models\Category;
+use Illuminate\Routing\Route as RoutingRoute;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\DashboardPostController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home', [
+        "active" => 'home',
+        "title" => "Home"
+
+    ]);
+});
+
+Route::get('/about', function () {
+    return view('about',[
+        "title" => "About",
+        "active" => 'about',
+        "name" => "Rafi Nuril Akbar F",
+        "email" => "rafinafirmansyah29@gmail.com",
+        "image" => "profile.JPEG"
+    ]);
+});
+
+
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('posts/{post:slug}', [PostController::class, 'show']);
+
+Route::get('/categories', function() {
+    return view('categories',[
+        'title' => 'Post Categories',
+        "active" => 'categories',
+        'categories' => Category::all()
+    ]);
+});
+
+
+// Route::get('/categories/{category:slug}', function(Category $category){
+//     return view('posts',[
+//         'title' => "Post By Category : $category->name",
+//         "active" => 'categories',
+//         'posts' => $category->posts->load('category', 'author'),
+//     ]);
+// });
+
+
+// Route::get('/authors/{author:username}', function(User $author){
+//     return view('posts',[
+//         'title' => "Post By Author : $author->name",
+//         'posts' => $author->posts->load('category', 'author'),
+//     ]);
+// });
+
+Route::get('/login', [LoginController::class, 'index'])->name('login')-> middleware('guest');
+Route::post('/login', [LoginController::class, 'authenticate']);
+Route::post('/logout', [LoginController::class, 'logout']);
+
+Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
+Route::post('/register', [RegisterController::class, 'store']);
+
+Route::get('/dashboard', function(){
+    return view('dashboard.index');
+})->middleware('auth');
+
+
+Route::get('/dashboard/posts/checkSlug', [DashboardPostController::class, 'checkSlug'])
+->middleware('auth');
+
+Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
